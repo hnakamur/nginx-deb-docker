070e71978876813a2734c1ef70e95d2a4f8fb5d6
Hiroaki Nakamura <hnakamur@gmail.com>
2026-06-06 15:07:55 +0900

    Remove naxsi

--- add_submodules.sh
+++ add_submodules.sh
@@ -52,7 +52,3 @@ git submodule add https://github.com/nginx/njs
 git submodule add https://github.com/owasp-modsecurity/ModSecurity-nginx
 git submodule add https://github.com/fffonion/lua-resty-openssl
 git submodule add https://github.com/matsumotory/ngx_mruby
-
-git submodule add https://github.com/wargio/naxsi
-(cd naxsi && git checkout 1.7)
-git submodule update --init --recursive naxsi
